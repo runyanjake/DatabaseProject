a468f22c610d479975b30f8949d6c77132baf52f
Jacob Runyan <jmrunyan@ucsc.edu>
2016-06-29 14:08:04 -1000

    Added listener functionality in the frame and title page.

--- DBTitlePage.java
+++ DBTitlePage.java
@@ -8,12 +8,19 @@
   */
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class DBTitlePage extends JPanel implements DBPage{
 	//**** FIELDS ****
 	static final long serialVersionUID = 1L;
+	JButton beginButton;
 
 	/**
 	  * Default No args constructor.
@@ -27,7 +34,14 @@ public class DBTitlePage extends JPanel implements DBPage{
 	  * Initializes this DBPage.
 	  */
 	public void setupPage(){
+		//set up page.
 		this.setBackground(Color.cyan);
+		this.setLayout(new BorderLayout());
+		//set up button
+		beginButton = new JButton();
+		beginButton.addActionListener(new startButtonListener());
+		this.add(beginButton, BorderLayout.CENTER);
+		//make visible
 		this.setVisible(true);
 	}
 
@@ -37,4 +51,15 @@ public class DBTitlePage extends JPanel implements DBPage{
 	public void updatePage(){
 
 	}
+
+	/**
+	  * Actionlistener for Button
+	  */
+	public class startButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			DBWindow frame = (DBWindow)SwingUtilities.getRoot(beginButton);
+			frame.attemptNextPage("login");
+		}
+	}
+
 }
--- DatabaseProject.java
+++ DatabaseProject.java
@@ -7,12 +7,46 @@
   *
   */
 
+import java.awt.event.WindowListener;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JOptionPane;
+
 public class DatabaseProject{
 
-	public static void main(String[] args){
-		DBWindow frame = new DBWindow("DataBaseProject");
+  DBWindow frame;
+
+  public static void main(String[] args){
+    DatabaseProject dbp = new DatabaseProject();
+  }
+
+  public DatabaseProject(){
+    initializeDB();
+  }
+
+  public void initializeDB(){
+    frame = new DBWindow("DataBaseProject");
+    frame.addWindowListener(new DBWindowListener());
     frame.setBounds(0,0, 400, 300);
     frame.setVisible(true);
-	}
+  }
+
+  public class DBWindowListener implements WindowListener{
+    public void windowClosing(WindowEvent e){
+      int a = JOptionPane.showConfirmDialog(frame, "Are you sure you would like to exit?", "Goodbye!", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+      if(a==1){
+        initializeDB();
+      }else{
+        System.exit(0);
+      }
+    }
+    public void windowDeactivated(WindowEvent e){}
+    public void windowOpened(WindowEvent e){}
+    public void windowClosed(WindowEvent e){}
+    public void windowIconified(WindowEvent e){}
+    public void windowDeiconified(WindowEvent e){}
+    public void windowActivated(WindowEvent e){}
+    
+  }
 
 }
